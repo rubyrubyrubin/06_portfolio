a40d05263a9f0ba6271064bce8e93979a8e285cc
Anna Rubin <anna91b7@stud.kea.dk>
2022-06-06 15:16:41 +0200

    first

---
+++ script/java.js
@@ -0,0 +1,7 @@
+const toggleButton = document.getElementById("toggle-button");
+const navList = document.getElementById("navi-list");
+
+toggleButton.addEventListener("click", () => {
+  navList.classList.toggle("active");
+  showcase.classList.toggle("active");
+});
