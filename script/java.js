a45f3f0caf82e4a18c645c470e9fecf50b473d4c
Anna Rubin <anna91b7@stud.kea.dk>
2022-06-15 00:04:50 +0200

    fucking menu

--- script/java.js
+++ script/java.js
@@ -20,9 +20,19 @@
 //   })
 // );
 
-const toggleButton = document.getElementById("toggle-button");
-const navList = document.getElementById("navi-list");
+// const toggleButton = document.getElementById("toggle-button");
+// const navList = document.getElementById("navi-list");
 
-toggleButton.addEventListener("click", () => {
-  navList.classList.toggle("active");
-});
+// toggleButton.addEventListener("click", () => {
+//   navList.classList.toggle("active");
+// });
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
