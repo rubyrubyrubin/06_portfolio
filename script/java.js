d1c207d946d444a3053f01d30f821ac7a226c251
Anna Rubin <anna91b7@stud.kea.dk>
2022-06-14 22:02:22 +0200

    webp

--- script/java.js
+++ script/java.js
@@ -1,3 +1,25 @@
+// const toggleButton = document.getElementById("toggle-button");
+// const navList = document.getElementById("navi-list");
+
+// toggleButton.addEventListener("click", () => {
+//   navList.classList.toggle("active");
+// });
+
+// const burger = document.querySelector(".burger");
+// const navMenu = document.querySelector(".nav-menu");
+
+// burger.addEventListener("click", () => {
+//   burger.classList.toggle("active");
+//   navMenu.classList.toggle("active");
+// });
+
+// document.querySelectorAll(".nav-link").forEach((n) =>
+//   n.addEventListener("click", () => {
+//     burger.classList.remove("active");
+//     navMenu.classList.remove("active");
+//   })
+// );
+
 const toggleButton = document.getElementById("toggle-button");
 const navList = document.getElementById("navi-list");
 
