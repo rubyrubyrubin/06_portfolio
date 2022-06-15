7282daae943fdbe846d300d12c2f280032065a53
Anna Rubin <anna91b7@stud.kea.dk>
2022-06-15 08:05:49 +0200

    menu responsiv

--- script/java.js
+++ script/java.js
@@ -1,42 +1,3 @@
-// const toggleButton = document.getElementById("toggle-button");
-// const navList = document.getElementById("navi-list");
-
-// toggleButton.addEventListener("click", () => {
-//   navList.classList.toggle("active");
-// });
-
-// const burger = document.querySelector(".burger");
-// const navMenu = document.querySelector(".nav-menu");
-
-// burger.addEventListener("click", () => {
-//   burger.classList.toggle("active");
-//   navMenu.classList.toggle("active");
-// });
-
-// document.querySelectorAll(".nav-link").forEach((n) =>
-//   n.addEventListener("click", () => {
-//     burger.classList.remove("active");
-//     navMenu.classList.remove("active");
-//   })
-// );
-
-// const toggleButton = document.getElementById("toggle-button");
-// const navList = document.getElementById("navi-list");
-
-// toggleButton.addEventListener("click", () => {
-//   navList.classList.toggle("active");
-// });
-
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-// function myFunction() {
-//   var x = document.getElementById("myLinks");
-//   if (x.style.display === "block") {
-//     x.style.display = "none";
-//   } else {
-//     x.style.display = "block";
-//   }
-// }
-
 var images = new Array();
 function preloadImages() {
   for (i = 0; i < preloadImages.arguments.length; i++) {
@@ -46,3 +7,12 @@ function preloadImages() {
   }
 }
 preloadImages("mig.png", "ligger.png", "v3_02.webp", "v1_02.webp", "v4_02.webp", "v2_02.webp", "crazy8_03.webp");
+
+// function myFunction() {
+//   var x = document.getElementById("myLinks");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
